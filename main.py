83b607b0dadaeb10a31072c47ae175e158a307d9
Lorenzo <60483161+LorenzoMarc@users.noreply.github.com>
2021-07-02 10:58:54 +0200

    Training_DQAgent

--- main.py
+++ main.py
@@ -1,51 +1,45 @@
 from gym import Env
 from gym.spaces import Discrete, Box
 import numpy as np
-import random
 import nnModel
+import random
+import math
+
 from tensorflow.keras.optimizers import Adam
 
 #########################################################################
 '''
- TODO: 
-    Task 1:
-    - Fix Seeker policy, movements are defined and his utilities too.
-    - Seeker KNOWS Hider's position
-    - Assume Hider knows where Seeker is
-    - Learn strategy to maximize HIDING
-    
-    Task 2: 
-    - Hider has fixed policy and Seeker has to find him
-    - Hider has random policy and Seeker has to find him
-    - Differences between the two results
-    
-    Extra Task:
-    - Explorative operations on maze with one/two approaches:
-        ° TOP-DOWN (DFS or BFS)
-        ° CLP   
-    - Possible graphical presentation with pygame and tutorial
+TASK: 
 
+Train a NN to resolve a maze resolution with Q Agent.
+The baseline environment is inherited from OpenAI Gym.
+Elements:
+- Custom Grid: to complete the environment
+- Step function: to proceed on training 
+- render function (not used)
+- reset function: bring state to the starting position
 '''
 
-
 # Env inherited from Open AI Gym
+# Grid is a representation of 5x5 dimension
 class Grid(Env):
 
     def __init__(self):
         # Actions: up, down, left, right
-        self.action_space = Discrete(4)
-        # grid cells
-        self.observation_space = Box(low=np.array([0, 0]), high=np.array([5,5]), dtype=int)
+        self.action_space = np.array([0, 1, 2, 3])
+        # grid cells as flatten array
+        self.observation_space = Box(low=np.array([0]), high=np.array([24]), dtype=int)
 
         # start position
-        self.state = np.array([0, 0])
+        self.state = np.array([0])
 
         # episode length
-        self.path_length = 10
+        self.path_length = 1000
+        # agent2 or maze's exit
+        self.target_state = np.array([24])
 
-        self.wall = False
-
-        self.target_state = np.array([5, 5])
+        # subset of walls
+        self.walls_states = [1, 6, 11, 13, 18, 23]
 
     def step(self, action):
         # action| path_length | state
@@ -54,35 +48,34 @@ class Grid(Env):
         # 2     |  -1         | (-1, _)
         # 3     |  -1         | (+1, _)
         if action == 0:
-            self.state[1] += 1
-            print(self.state)
+            self.state -= 5
+
         if action == 1:
-            self.state[1] -= 1
-            print(self.state)
+            self.state += 5
+
         if action == 2:
-            self.state[0] -= 1
-            print(self.state)
+            self.state -= 1
+
         if action == 3:
-            self.state[0] += 1
-            print(self.state)
+            self.state += 1
 
         # Reduce path length by 1
         self.path_length -= 1
 
-
-        # Check this distance. Choose appropriate distance measure
-        target_distance = np.linalg.norm(self.state - self.target_state)
-
-        # Calculate reward
-        if self.state[0] == self.target_state[0] and self.state[1] == self.target_state[1]:
-            reward = 100
-        elif target_distance <= 2:
-            reward = 1
-        else:
+        # Calculate reward if target state is reached
+        if self.state == self.target_state:
+            reward = 500
+        # decrease reward if a wall is hit
+        elif [self.state == x for x in self.walls_states]:
             reward = -1
+        # decrease reward if negative state(out of env)
+        elif self.state < 0:
+            reward = -1
+        else:
+            reward = 0
 
-            # Check if search is done
-        if self.path_length <= 0:
+        # Check if episode is done
+        if self.path_length <= 0 or self.state == self.target_state:
             done = True
         else:
             done = False
@@ -99,14 +92,17 @@ class Grid(Env):
 
     def reset(self):
         # Reset path temperature
-        self.state = np.array([0, 0])
+        self.state = np.array(0)
         # Reset path
         self.path_length = 10
         return self.state
 
+
 env = Grid()
 
-episodes = 10
+'''
+# Random walk on maze in 100 episodes
+episodes = 100
 
 for episode in range(episodes):
     state = env.reset()
@@ -115,23 +111,41 @@ for episode in range(episodes):
 
     while not done:
         env.render()
-        action = env.action_space.sample()
+        if state in [1, 2, 3]:
+            env.action_space = np.array([1 ,2 ,3])
+        elif state in [5, 10, 15]:
+            env.action_space = np.array([0,1,3])
+        elif state in [9, 14, 19]:
+            env.action_space = np.array([0,1,2])
+        elif state in [21, 22, 23]:
+            env.action_space = np.array([0,2,3])
+        elif state == 0:
+            env.action_space = np.array([1,3])
+        elif state == 4:
+            env.action_space = np.array([1,2])
+        elif state == 20:
+            env.action_space = np.array([0,3])
+        elif state == 24:
+            env.action_space = np.array([0,2])
+        else:
+            env.action_space = np.array([0,1,2,3])
+        action = random.choice(env.action_space)
         n_state, reward, done, info = env.step(action)
         score += reward
-    print('episode:{} Score:{}'.format(episode, score))
+    #print('episode:{} Score:{}'.format(episode, score))
+'''
+# Input for NN model and agent
+states = env.observation_space.shape
+actions = len(env.action_space)
+# Dense Net is build with status
+model = nnModel.build_model(states, actions)
+model.summary()
 
-
-x, y = env.observation_space.sample()
-print(x,y)
-exit(1)
-actions = env.action_space.n
-print(actions)
-
-model = nnModel.build_model(x, y, actions)
-print(model)
-
-
+# Deep Q agent is built, compiled with the network
+# and fitted in the env. It have been trained for 50000 steps.
+# Test scores are calculated in 100 episodes
 dqn = nnModel.build_agent(model, actions)
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
-dqn.fit(env, nb_steps=50000, visualize=False, verbose=1)
-
+dqn.fit(env, nb_steps=5000, visualize=False, verbose=1)
+scores = dqn.test(env, nb_episodes=100, visualize=False)
+print(np.mean(scores.history['episode_reward']))
--- nnModel.py
+++ nnModel.py
@@ -1,26 +1,26 @@
 import numpy
 import tensorflow
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
-
-
+from tensorflow.keras.layers import Dense
 from rl.agents import DQNAgent
 from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
-from tensorflow.python.keras.layers import Reshape
 
-
-def build_model(x,y, actions):
+# The network model is a dense type, so each layer is fully connected with the others
+# The first two layers has ReLU activation function.
+# The output layer has Linear function, that return an array with 4 probs of 4 actions.
+def build_model(states, actions):
     model = Sequential()
-    model.add(Flatten(input_shape=(2, x, y)))
-    model.add(Dense(64, activation='relu'))
+    model.add(Dense(32, activation='relu', input_shape=states))
     model.add(Dense(32, activation='relu'))
     model.add(Dense(actions, activation='linear'))
     return model
 
-
-def build_agent(model,actions):
+# Build agent with policy
+# return a trained agent with Q function
+def build_agent(model, actions):
     policy = BoltzmannQPolicy()
-    memory = SequentialMemory(limit=50000, window_length=1)
-    dqn = DQNAgent(model=model, memory=memory, policy=policy, nb_actions=actions, nb_steps_warmup=10, target_model_update = 1e-2)
+    memory = SequentialMemory(limit=5000, window_length=1)
+    dqn = DQNAgent(model=model, memory=memory, policy=policy, nb_actions=actions, nb_steps_warmup=1000,
+                   target_model_update=1e-2)
     return dqn
